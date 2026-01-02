327fe87b51e687b2cad00fbc8598a04ec7ffe7cc
Jared <jaredtponting@gmail.com>
2026-01-02 00:16:11 +0000

    url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/";
+
 
 export const apiFetch = async (url: string, options: any = {}) => {
     const headers = {
