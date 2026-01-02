4074e875b77c385013c69e71cafc84d600e23e0b
Jared <jaredtponting@gmail.com>
2026-01-02 12:57:27 +0000

    api key required

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_URL ;
 export const apiFetch = async (url: string, options: any = {}) => {
     const headers = {
         "Content-Type": "application/json",
+        "X-API-Key": import.meta.env.VITE_API_KEY,
         ...options.headers,
     };
 
